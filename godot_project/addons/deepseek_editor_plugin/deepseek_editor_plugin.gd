a555b494a84419a5873f9af6858d66afab9d5521
aidoruao <aidoruao@gmail.com>
2026-07-23 17:38:23 -0500

    fix: remove redundant DeepSeekConfig autoload, fix RefCounted singleton warning in editor plugin

---
+++ godot_project/addons/deepseek_editor_plugin/deepseek_editor_plugin.gd
@@ -0,0 +1,20 @@
+@tool
+extends EditorPlugin
+
+var client: DeepSeekClient
+
+func _enter_tree():
+    print("[DeepSeekEditorPlugin] Injecting API key...")
+    var env_key = OS.get_environment("DEEPSEEK_API_KEY")
+    if env_key.is_empty():
+        push_warning("[DeepSeekEditorPlugin] DEEPSEEK_API_KEY not set")
+        return
+    client = DeepSeekClient.new()
+    client.set_api_key(env_key)
+    set_meta("deepseek_client", client)
+    print("[DeepSeekEditorPlugin] API key injected (", env_key.length(), " chars)")
+
+func _exit_tree():
+    if client:
+        client.free()
+        client = null
